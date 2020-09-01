7c6127e1a54c35a4088aa73b47522cc487d12613
isengupt <ishan6060@gmail.com>
2020-09-01 01:17:51 -0400

    revised css problems

--- imports/ui/pages/About/AboutPage.jsx
+++ imports/ui/pages/About/AboutPage.jsx
@@ -1,90 +1,83 @@
-import React, { useState, useEffect } from 'react';
-import { DetailComponents,  setButtonState, useAccount } from "../../utils/utils"
-import Header from './components/Header'
-import Leaders from './components/Leaders'
-import Intiatives from './components/Initiatives'
-import Inquire from './components/Inquire'
+import React, { useState, useEffect } from "react";
+import {
+  DetailComponents,
+  setButtonState,
+  useAccount,
+} from "../../utils/utils";
+import Header from "./components/Header";
+import Leaders from "./components/Leaders";
+import Intiatives from "./components/Initiatives";
+import Inquire from "./components/Inquire";
 import ReactPlayer from "react-player";
-
+import Reveal from "react-reveal/Reveal";
 
 
 const AboutPage = () => {
   const { user, isLoggingIn } = useAccount();
   const [aboutData, setAboutData] = useState(false);
- 
-
 
   useEffect(() => {
     Meteor.call("getCurrentAboutData", (e, r) => {
       if (!e) setAboutData(r);
-      console.log(r)
-
-
+      console.log(r);
     });
   }, []);
 
   useEffect(() => {
     Meteor.call("setEditable", "about", (e, r) => {
-
       if (!e) {
-
-        if(r) {
-          setButtonState("edit-about")
-        }
-        else setButtonState("dashboard")
+        if (r) {
+          setButtonState("edit-about");
+        } else setButtonState("dashboard");
       }
-
     });
-  }, [user, isLoggingIn])
-
-
+  }, [user, isLoggingIn]);
 
   return (
     <>
-      {aboutData ?
+      {aboutData ? (
         <>
+          <Reveal effect="fadeInUp">
+            <Header />
 
-        <Header/>
+            <div class="content-section-bg">
+              <div class="initiatives-header no-margin-bottom green-text">
+                <div class="title-wrap-centre extra-bottom">
+                  <h2 class="heading-2 big-heading-white">
+                    See what we're about
+                  </h2>
+                  <p class="paragraph-2 no-margin-title">See what drives us.</p>
+                  <a
+                    href="order.html"
+                    data-w-id="4b208bba-bbb5-5afa-238f-737f9161a635"
+                    class="learn-more-button-green w-button"
+                  >
+                    Learn More
+                  </a>
+                </div>
+                <ReactPlayer
+                  width="100%"
+                  height="560px"
+                  url="https://www.youtube.com/watch?v=aBa6cbg0hH4&feature=emb_title"
+                />
+              </div>
+            </div>
 
-        <div class="content-section-bg">
-        <div class="initiatives-header no-margin-bottom green-text">
-          <div class="title-wrap-centre extra-bottom">
-            <h2 class="heading-2 big-heading-white">See what we're about</h2>
-            <p class="paragraph-2 no-margin-title">See what drives us.</p>
-            <a
-            href="order.html"
-            data-w-id="4b208bba-bbb5-5afa-238f-737f9161a635"
-            class="learn-more-button w-button"
-          >
-            Learn More
-          </a>
-        
-          </div>
-          <ReactPlayer width="100%" height="560px"
-        url="https://www.youtube.com/watch?v=aBa6cbg0hH4&feature=emb_title"
-      />
-        </div>
-       
-      </div>
-       
-        <Leaders/>
-        <Intiatives/>
-        <Inquire/>
+            <Leaders />
+            <Intiatives />
+            <Inquire />
+          </Reveal>
 
-     {/*     <DetailComponents.ImageGalleryDetail detailInfo={aboutData.images}/>
+          {/*     <DetailComponents.ImageGalleryDetail detailInfo={aboutData.images}/>
           <DetailComponents.StoryDetail detailInfo={aboutData.story}/>
           <DetailComponents.MissionDetail detailInfo={aboutData.mission}/>
           <DetailComponents.InitiativesDetail detailInfo={aboutData.initiatives}/>  */}
         </>
-        :
-        <>
-        </>
-
-      }
-
-
+      ) : (
+        <></>
+      )}
     </>
-  )
-}
+  );
+};
 
 export default AboutPage;
--- imports/ui/pages/About/components/Initiatives.jsx
+++ imports/ui/pages/About/components/Initiatives.jsx
@@ -1,7 +1,15 @@
 import React, { useState, useEffect } from "react";
-import ImageGallery from 'react-image-gallery';
+
+import Carousel from 'react-bootstrap/Carousel'
 
 const Initiatives = () => {
+
+  const [index, setIndex] = useState(1);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
   return (
     <>
       <div class="content-section-bg">
@@ -27,9 +35,9 @@ const Initiatives = () => {
        
           <div id="w-node-f228809eb4a3-a378b556" class="div-block-7">
             <div class="iniatives-home-page-image"
-            style={{backgroundImage: "url(images/leader1.jpg)"}}
+            style={{backgroundImage: "url(images/updates1.JPG)"}}
             >
-
+   
               
             </div>
             <div class="iniatives-home-page-container">
@@ -45,7 +53,36 @@ const Initiatives = () => {
             </div>
           </div>
           <div id="w-node-639d81b3f566-a378b556" class="div-block-7">
-            <div class="iniatives-home-page-image"></div>
+            <div class="iniatives-home-page-image">
+          
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="w-100"
+          src="images/updates4.jpeg"
+          alt="First slide"
+        />
+   
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="w-100"
+          src="images/updates2.JPG"
+          alt="Second slide"
+        />
+
+
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="w-100"
+          src="images/updates3.JPG"
+          alt="Third slide"
+        />
+
+      </Carousel.Item>
+    </Carousel>
+            </div>
             <div class="iniatives-home-page-container">
               <div class="home-page-initiatives-text">
                 <div class="iniatives-heading">We are committed.</div>
@@ -62,6 +99,7 @@ const Initiatives = () => {
             <div
               id="w-node-1b856dc98d58-a378b556"
               class="iniatives-home-page-image"
+              style={{backgroundImage: "url(images/updates5.jpg)"}}
             ></div>
             <div class="iniatives-home-page-container">
               <div class="home-page-initiatives-text">
